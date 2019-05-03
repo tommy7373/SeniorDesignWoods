4c7f9687c13598a52e61dcd80c9ca1ca9be7eb8b
tommy7373 <thomas.shipman@mavs.uta.edu>
2019-05-02 21:13:44 -0500

    Code wth averaging
    Averaging filter for all 3 accel axis. Can be reused for gyro if so desired.

--- main_proc/main_proc.ino
+++ main_proc/main_proc.ino
@@ -18,20 +18,40 @@
 #endif
 
 MPU9250_DMP imu;
-Adafruit_7segment green_seg = Adafruit_7segment();
+//  Adafruit_7segment green_seg = Adafruit_7segment();
 Servo myservo;
 
 unsigned long currMs = 0;
 unsigned long lastUpdateMs;
-int gyro_x, gyro_y, gyro_z;
-int accel_x, accel_y, accel_z;
+
+/////////////////////
+// SD Card Globals //
+/////////////////////
+bool sdCardPresent = false; // Keeps track of if SD card is plugged in
+String logFileName = "mainlog.csv"; // Active logging file
+String logFileBuffer; // Buffer for logged data. Max is set in config
+
+const int numReadings = 75; // Number of samples to take for averaging
+
+int readings_X[numReadings]; // Variables for averaging
+int readIndex_X = 0;
+int total_X = 0;
+int average_X = 0;
+int readings_Y[numReadings];
+int readIndex_Y = 0;
+int total_Y = 0;
+int average_Y = 0;
+int readings_Z[numReadings];
+int readIndex_Z = 0;
+int total_Z = 0;
+int average_Z = 0;
 
 int pos = 0;
 
 //Setup for poll rates
 unsigned short accelFSR = 16; // Set accel full-scale range: options are 2, 4, 8, or 16 g 
-unsigned short gyroFSR = 250; // Set gyro full-scale range: options are 250, 500, 1000, or 2000 deg/s
-unsigned short UpdateRate = 100; // Set DMP/IMU update rate, between 4-200Hz
+unsigned short gyroFSR = 2000; // Set gyro full-scale range: options are 250, 500, 1000, or 2000 deg/s
+unsigned short UpdateRate = 200; // Set DMP/IMU update rate, between 4-200Hz
 unsigned short imuLPF = 5; // Set gyro/accel LPF: options are 5, 10, 20, 42, 98, 188 Hz
 
 void setup() {
@@ -39,7 +59,11 @@ void setup() {
   pinMode(MPU9250_INT_PIN, INPUT_PULLUP);
   pinMode(HW_LED_PIN, OUTPUT);
   digitalWrite(HW_LED_PIN, LOW);
-  myservo.attach(10);
+  if(initSD())
+  {
+    sdCardPresent = true;
+  }
+  myservo.attach(10); // Servo attached on pin 10
 
   initIMU();
 }
@@ -48,41 +72,39 @@ void loop() {
   currMs = millis();
   imu.dmpUpdateFifo();
   lastUpdateMs = currMs;
-  accel_x = imu.ax;
-  accel_y = imu.ay;
-  accel_z = imu.az;
-  gyro_x = imu.gx;
-  gyro_y = imu.gy;
-  gyro_z = imu.gz;
-  pos = abs(gyro_z/182)+1;
+  
+  total_X = total_X - readings_X[readIndex_X]; // Calculating average for the X axis
+  readings_X[readIndex_X] = imu.ax;
+  total_X = total_X + readings_X[readIndex_X];
+  readIndex_X = readIndex_X+1;
+  if (readIndex_X >= numReadings){
+    readIndex_X=0;
+  }
+  average_X = total_X / numReadings;
+
+  total_Y = total_Y - readings_Y[readIndex_Y]; // Calculating average for the Y axis
+  readings_Y[readIndex_Y] = imu.ay;
+  total_Y = total_Y + readings_Y[readIndex_Y];
+  readIndex_Y = readIndex_Y+1;
+  if (readIndex_Y >= numReadings){
+    readIndex_Y=0;
+  }
+  average_Y = total_Y / numReadings;
+
+  total_Z = total_Z - readings_Z[readIndex_Z]; // Calculating average for the Z axis
+  readings_Z[readIndex_Z] = imu.az;
+  total_Z = total_Z + readings_Z[readIndex_Z];
+  readIndex_Z = readIndex_Z+1;
+  if (readIndex_Z >= numReadings){
+    readIndex_Z=0;
+  }
+  average_Z = total_Z / numReadings;
+  
+  pos = abs(average_X/182)+1;
   myservo.write(pos);
+  
   logData();
   delay(5);
-  
-//  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(5);                       // waits 15ms for the servo to reach the position
-//  }
-//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(5);                       // waits 15ms for the servo to reach the position
-//  }
-  
-//  if (currMs > lastUpdateMs) {
-//    imu.dmpUpdateFifo();
-//    lastUpdateMs = currMs;
-//    accel_x = imu.ax;
-//    accel_y = imu.ay;
-//    accel_z = imu.az;
-//    gyro_x = imu.gx;
-//    gyro_y = imu.gy;
-//    gyro_z = imu.gz;
-//    green_seg.print(imu.calcAccel(accel_y),DEC);
-//    
-//    green_seg.writeDisplay();
-//  }
-
 }
 
 bool initIMU(void)
@@ -124,10 +146,28 @@ bool initIMU(void)
 void logData(void)
 {
   String imuLog = "";
-  imuLog += "Gyro (x,y,z): ";
-  imuLog += String(imu.gx) + ", ";
-  imuLog += String(imu.gy) + ", ";
-  imuLog += String(imu.gz) + ", ";
+  //imuLog += "Accel (x,y,z): ";
+  imuLog += String(average_X) + ", "; // Logging using csv format
+  imuLog += String(average_Y) + ", ";
+  imuLog += String(average_Z) + ", ";
   imuLog += "\r\n";
-  SERIAL_PORT_USBVIRTUAL.print(imuLog);
+//  if(sdCardPresent = true){
+//    File logFile = SD.open(logFileName, FILE_WRITE);
+//    if(logFile){
+//      logFile.print(imuLog);
+//      logFile.close();
+//    }
+//  }
+  SERIAL_PORT_USBVIRTUAL.print(imuLog); // Printing to USB port serial
+}
+
+bool initSD(void)
+{
+  // SD.begin should return true if a valid SD card is present
+  if ( !SD.begin(38) )
+  {
+    return false;
+  }
+
+  return true;
 }
